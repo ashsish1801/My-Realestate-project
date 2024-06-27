a58677ae1880e4ca45458caacfaa4d7f9f88ed01
Ashish <ashishagrawal0181@gmail.com>
2024-06-27 19:00:49 +0530

    complete image upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom' 
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 export default function Header() {
   const {currentUser} = useSelector(state => state.user)
+  useEffect(()=>{
+    console.log("CurrentUser from Header: ", currentUser);
+  },[currentUser])
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
